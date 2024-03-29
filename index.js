620019b2a8971da9fe1410a8b3186d847d4cf775
User <diyorbekabduraupov208@gmail.com>
2024-03-29 21:20:15 +0500

    go

---
+++ index.js
@@ -0,0 +1,16 @@
+let a = prompt ("Tug'ilgan yilingizni kiriting");
+let b = 2024;
+
+if( b - a > 0 ){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+let c = b - a ;
+
+console.log("Siz "+ c +" yoshdasiz" );
+
+document.body.innerHTML = "Siz "+ c +" yoshdasiz" 
+
+
